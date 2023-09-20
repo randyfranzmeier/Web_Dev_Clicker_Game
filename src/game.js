16f5220ce64945aab50e7b9845dafe8d26193dfc
Randy.F <randy.franzmeier@my.uwrf.edu>
2023-09-20 11:01:24 -0500

    getting ui set up

---
+++ src/game.js
@@ -0,0 +1,90 @@
+//this file deals with the main logic for the game
+
+//declare boolean variables for power ups
+
+    let displayPoints = document.getElementById("points");
+     points = 0;
+     clicks = 0;
+
+    let handleClick = () =>{
+         clicks++;
+         setPoints(points + 1);
+         displayUserPoints();
+
+     }
+
+     
+     let getPoints = () =>{
+         return points;
+        }
+        
+        let setPoints = (points) =>{
+            this.points = points;
+        }
+        
+        let displayUserPoints = () => {
+            displayPoints.innerHTML = "POINTS: " + getPoints();
+            if(getPoints() >= 50000) {displayPoints.style.color = "gold";}
+            else {displayPoints.style.color = "white";}
+        }
+
+        //------------------------------Store------------------------------------------
+        //store contains functions pertaining to the store section of the page
+        let store = {
+            purchaseItem (num) {
+                //switch statement, each number correlates to button pressed?
+                
+            }
+
+        }
+
+
+        //------------------------------Achievements------------------------------------
+        //achievements object to contain functions for achievements portion of page
+        let achievements = {
+
+        }
+        
+    
+      
+    
+    
+
+/*class driver { 
+    displayPoints = document.getElementById("points");
+    points = 0;
+
+
+    gameLoaded = () =>{
+        if (getPoints() > 0) {
+            alert("Are you sure you want to lose all your points?");
+        }
+    }
+
+    getPoints = () =>{
+        return points;
+    }
+
+    setPoints = (points) =>{
+        this.points = points;
+    }
+
+   displayUserPoints = () => {
+        displayPoints.innerHTML = "POINTS: + ${getPoits}";
+    }
+
+}
+class game {
+    displayPoints = document.getElementById("points");
+    constructor(points){this.points = points;}
+    
+    handleClick = () =>{
+        setPoints(points + 1);
+        displayUserPoints();
+    }
+    
+    
+    
+}
+*/
+
